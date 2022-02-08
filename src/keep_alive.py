1744894b4dfa0f7a0f0b7de4f4440df8a7b352c1
K1ngAG <88247183+kingdevag@users.noreply.github.com>
2022-02-08 22:48:38 +0000

    New web

--- src/keep_alive.py
+++ src/keep_alive.py
@@ -8,18 +8,6 @@ app = Flask('')
 @app.route('/')
 def main():
     return render_template("index.html")
-  
-@app.route('/comandos')
-def commans():
-    return render_template("commands.html")
-  
-@app.route('/social')
-def social():
-    return render_template("social.html")
-  
-@app.route('/support')
-def support():
-    return render_template("support.html")
 
 def run():
   app.run(host="0.0.0.0", port=8101)
